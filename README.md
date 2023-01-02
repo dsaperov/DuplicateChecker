c8ec2b82d27c76d43242f1174eddbf2e887ec08a
dsaperov <dsaperov.md@gmail.com>
2023-01-02 15:39:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# What is DuplicateChecker?
+A script to search for files of **specified extension** with duplicate content in the **specified folder**.
+Outputs a list of filenames having fully identical content. Each group of filenames with identical content is separated 
+from another via dashed line (`----------------------`). So the output is like the following:
+```
+file_with_content_A_1.txt
+file_with_content_A_2.txt
+file_with_content_A_3.txt
+----------------------
+file_with_content_B_1.txt
+file_with_content_B_2.txt
+```
+
+# Usage
+There are **2 positional arguments** you should specify to run the script:
+- `path` - path to the folder you need to check for files with duplicate content in (*example:* `C:/Users/My_folder`)
+- `extension` - extension (*without a dot*) of files to be checked (*example:* `txt`, but not `.txt`)
+
+## Example of usage:
+```bash
+python main.py C:/Users/My_folder txt
+```
+
+
+
---
+++ main.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+from collections import defaultdict
+
+parser = argparse.ArgumentParser(epilog='Example of use: python main.py C:/Users/My_folder txt')
+parser.add_argument('path',
+                    help='Path to the folder you need to check for files with duplicate content in (example: "C:/Users/'
+                         'My_folder")')
+parser.add_argument('extension',
+                    help='Extension (without a dot) of files to be checked (example: "txt", but not ".txt")')
+args = parser.parse_args()
+path = os.path.normpath(args.path)
+extension = '.' + os.path.normpath(args.extension)
+
+filenames_for_hashes = defaultdict(list)
+hashes_of_duplicates = set()
+
+
+def get_hash(filename):
+    with open(filename, mode='rb') as file:
+        file_content = file.read()
+        return hash(file_content)
+
+
+for name in os.listdir(path):
+    f = os.path.join(path, name)
+    if os.path.isfile(f) and f.lower().endswith(extension):
+        content_hash = get_hash(f)
+        if content_hash in filenames_for_hashes:
+            hashes_of_duplicates.add(content_hash)
+        filenames_for_hashes[content_hash].append(name)
+
+if hashes_of_duplicates:
+    print('Files with duplicate content found:')
+    last_hash_number = len(hashes_of_duplicates)
+    for i, hash_value in enumerate(hashes_of_duplicates, 1):
+        print(*filenames_for_hashes[hash_value], sep='\n')
+        if i != last_hash_number:
+            print('----------------------')
+else:
+    print('No files with duplicate content found')
