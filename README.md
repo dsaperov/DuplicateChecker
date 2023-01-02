fefb5ecd56e46e01d6589f723591c4166927c69a
dsaperov <dsaperov.md@gmail.com>
2023-01-02 18:27:55 +0300

    Make extension an optional argument

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # What is DuplicateChecker?
-A script to search for files of **specified extension** with duplicate content in the **specified folder**.
+A script to search for files with duplicate content in the **specified folder**. Can be configured to check only files 
+of **specified extension**. 
 Outputs a list of filenames having fully identical content. Each group of filenames with identical content is separated 
 from another via dashed line (`----------------------`). So the output is like the following:
 ```
@@ -12,14 +13,15 @@ file_with_content_B_2.txt
 ```
 
 # Usage
-There are **2 positional arguments** you should specify to run the script:
-- `path` - path to the folder you need to check for files with duplicate content in (*example:* `"C:/Users/My_folder"`)
-- `extension` - extension (*without a dot*) of files to be checked (*example:* `txt`, but not `.txt`)
+The script accepts the following arguments:
+- `path` (**positional**) - path to the folder you need to check for files with duplicate content in. Should be 
+  enclosed in double quotes (*example:* `"C:/Users/My_folder"`).
+- `-e, --extension` (**optional**) - extension (*without a dot*) of files to be checked (*example:* `txt`, but not 
+  `.txt`). If not specified, files with all extensions will be checked.
 
-## Example of usage:
-```bash
-python main.py "C:/Users/My_folder" txt
-```
+## Examples of usage:
+- `python main.py "C:/Users/My_folder"` to check files with all extensions.
+- `python main.py "C:/Users/My_folder" -e txt` to check only files with `.txt` extension.
 
 
 
--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import argparse
 import os
 from collections import defaultdict
 
-parser = argparse.ArgumentParser(epilog='Example of use: python main.py "C:/Users/My_folder" txt')
+parser = argparse.ArgumentParser(epilog='Examples of use:\n'
+                                        '- python main.py "C:/Users/My_folder" (to check files of all extensions)\n'
+                                        '- python main.py "C:/Users/My_folder" -e txt (to check only files of .txt '
+                                        'extension)',
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument('path',
-                    help='Path to the folder you need to check for files with duplicate content in (example: "C:/Users/'
-                         'My_folder")')
-parser.add_argument('extension',
-                    help='Extension (without a dot) of files to be checked (example: "txt", but not ".txt")')
+                    help='path to the folder you need to check for files with duplicate content in. Should be enclosed '
+                         'in double quotes (example: "C:/Users/My_folder").')
+parser.add_argument('-e', '--extension',
+                    help='extension (without a dot) of files to be checked (example: "txt", but not ".txt"). If not '
+                         'specified, files of all extensions will be checked.')
 args = parser.parse_args()
 path = os.path.normpath(args.path)
-extension = '.' + os.path.normpath(args.extension)
+extension = '.' + os.path.normpath(args.extension) if args.extension else None
 
 filenames_for_hashes = defaultdict(list)
 hashes_of_duplicates = set()
@@ -24,7 +29,9 @@ def get_hash(filename):
 
 for name in os.listdir(path):
     f = os.path.join(path, name)
-    if os.path.isfile(f) and f.lower().endswith(extension):
+    if os.path.isfile(f):
+        if extension and not f.lower().endswith(extension):
+            continue
         content_hash = get_hash(f)
         if content_hash in filenames_for_hashes:
             hashes_of_duplicates.add(content_hash)
